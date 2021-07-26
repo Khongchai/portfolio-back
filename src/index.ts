7b87828c4e0061d8393f0e8544348fe11a6bcee2
khongchai <world1955@hotmail.com>
2021-07-26 23:45:22 +0700

    added newrelic monitor

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+require("newrelic");
 import { buildSchema } from "type-graphql";
 import express from "express";
 import dotenv from "dotenv";
--- src/migrations/1626693168780-migrateStuff.ts
+++ src/migrations/1626693168780-migrateStuff.ts
@@ -1,11 +1,52 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class migrateStuff1626693168795 implements MigrationInterface {
+export class migrateStuff1626693168796 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     //Delete from project_entity;
     queryRunner.query(`
 
     delete from project_entity;
+    delete from technology_entity;
+
+    alter sequence technology_entity_id_seq restart with 1;
+insert into technology_entity (title) values ('TypeORM');
+insert into technology_entity (title) values ('MikroORM');
+insert into technology_entity (title) values ('Docker');
+insert into technology_entity (title) values ('Github');
+insert into technology_entity (title) values ('jQuery');
+insert into technology_entity (title) values ('Java');
+insert into technology_entity (title) values ('Sass');
+insert into technology_entity (title) values ('PostgreSQL');
+insert into technology_entity (title) values ('GitHub Pages');
+insert into technology_entity (title) values ('TypeScript');
+insert into technology_entity (title) values ('React');
+insert into technology_entity (title) values ('Redux');
+insert into technology_entity (title) values ('Android');
+insert into technology_entity (title) values ('Express');
+insert into technology_entity (title) values ('Django');
+insert into technology_entity (title) values ('Next.js');
+insert into technology_entity (title) values ('Heroku');
+insert into technology_entity (title) values ('Gatsby');
+insert into technology_entity (title) values ('styled-components');
+insert into technology_entity (title) values ('Chakra UI');
+insert into technology_entity (title) values ('Python');
+insert into technology_entity (title) values ('Javascript');
+insert into technology_entity (title) values ('Redis');
+insert into technology_entity (title) values ('Apollo');
+insert into technology_entity (title) values ('Urql');
+insert into technology_entity (title) values ('Node');
+insert into technology_entity (title) values ('TypeGraphQL');
+insert into technology_entity (title) values ('GraphQL'); 
+insert into technology_entity (title) values ('SQL');
+insert into technology_entity (title) values ('Google Play');
+insert into technology_entity (title) values ('Three.js');
+insert into technology_entity (title) values ('Go');
+insert into technology_entity (title) values ('Vercel');
+insert into technology_entity (title) values ('GLSL');
+insert into technology_entity (title) values ('Graphene-Python');
+insert into technology_entity (title) values ('Cypress');
+insert into technology_entity (title) values ('React Testing Library');
+insert into technology_entity (title) values ('Jest');
 
     alter sequence project_entity_id_seq restart with 1;
     insert into project_entity (title, description, "shortDescription", "githubLink", "websiteLink", "startDate", "endDate", "isHighlight", "imgLink", "tinyImgLink", "heroImgLink", "playStoreLink") 
