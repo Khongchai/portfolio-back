aa0f4c3a17438e6c18e1eab9bc083eb5abee3aa6
khongchai <world1955@hotmail.com>
2023-01-31 23:31:20 +0700

    removed dist folder from git

--- dist/constants.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.COOKIE_NAME = exports.IN_PRODUCTION = void 0;
-exports.IN_PRODUCTION = process.env.NODE_ENV === "production";
-exports.COOKIE_NAME = "qid";
-//# sourceMappingURL=constants.js.map
